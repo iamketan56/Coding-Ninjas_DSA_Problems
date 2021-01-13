041c5ac63c60c54e79f7cfa390f8f22a7cf2325c
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-01-13 07:41:56 +0530

    Create First_Occurence_of_a_number.cpp

---
+++ Sorting and Searching/First_Occurence_of_a_number.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int firstoccurece(int *a, int s, int e, int key)
+{
+
+    int mid = (s + e) / 2;
+    int ans = -1;
+    while (s <= e)
+    {
+        mid = (s + e) / 2;
+        if (a[mid] > key)
+        {
+            e = mid - 1;
+        }
+        if (a[mid] < key)
+        {
+            s = mid + 1;
+        }
+        if (a[mid] == key)
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    int a[] = {1, 2, 2, 2, 3, 4, 4};
+    int key;
+    cout << "Enter the number to find the first occurence location" << endl;
+    cin >> key;
+    cout << "First Cccurence of " << key << " is " << firstoccurece(a, 0, 6, key);
+}
