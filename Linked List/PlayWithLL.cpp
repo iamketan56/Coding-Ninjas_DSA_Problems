59def5a3c29d1d015301e84ca99781e6523281ec
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-01-24 07:55:57 +0530

    Create PlayWithLL.cpp

---
+++ Linked List/PlayWithLL.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+Node *takeinput()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while (data != -1)
+    {
+        Node *newnode = new Node(data);
+        if (head == NULL)
+        {
+            head = newnode;
+            tail = newnode;
+        }
+        else
+        {
+            tail = newnode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+int main()
+{
+    Node *head = takeinput();
+    print(head);
+}
