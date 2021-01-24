69f6842534582b0107e049c9fa850da230868474
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-01-24 08:16:11 +0530

    Create InsertSomthing.cpp

---
+++ Linked List/InsertSomthing.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+Node *takeinput()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while (data != -1)
+    {
+        Node *newnode = new Node(data);
+        if (head == NULL)
+        {
+            head = newnode;
+            tail = newnode;
+        }
+        else
+        {
+            tail->next = newnode;
+            tail = tail->next;
+        }
+        cin >> data;
+    }
+    return head;
+}
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+void insert(Node *head, int data, int positon)
+{
+    Node *temp = head;
+    Node *newnode = new Node(data);
+    int pos = 0;
+    while (pos < positon - 1)
+    {
+        temp = temp->next;
+        pos++;
+    }
+
+    Node *a = temp->next;
+    temp->next = newnode;
+    newnode->next = a;
+}
+int main()
+{
+    Node *head = takeinput();
+    print(head);
+    int data;
+    int position;
+    cout << "Enter element and postion before where you want to insert" << endl;
+    cin >> data >> position;
+    insert(head, data, position);
+    print(head);
+}
