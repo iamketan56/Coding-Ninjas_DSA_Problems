2dd3564c33cfcffd4b8cc6a26b20fd0167e2943a
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-01-25 08:08:49 +0530

    Create Find_Mid_Point.cpp

---
+++ Linked List/Find_Mid_Point.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+Node *takeinput()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while (data != -1)
+    {
+        Node *newnode = new Node(data);
+        if (head == NULL)
+        {
+            head = newnode;
+            tail = newnode;
+        }
+        else
+        {
+            tail->next = newnode;
+            tail = tail->next;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+Node *Midpoint(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head->next;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+int main()
+{
+    Node *head = takeinput();
+    cout << "Mid point is" << endl;
+    head = Midpoint(head);
+    cout << head->data;
+}
