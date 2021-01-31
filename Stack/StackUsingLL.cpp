13f90096435345f43c36ac1fcb87dc8b6d6c8174
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-01-31 08:28:12 +0530

    Create StackUsingLL.cpp

---
+++ Stack/StackUsingLL.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+Node *head = NULL;
+int size = 0;
+void push(int data)
+{
+
+    Node *newnode = new Node(data);
+    if (head == NULL)
+    {
+        head = newnode;
+    }
+    else
+    {
+        newnode->next = head;
+        head = newnode;
+    }
+    size++;
+}
+int pop()
+{
+    int ans = head->data;
+    head = head->next;
+    size--;
+    return ans;
+}
+bool empty()
+{
+    return size == 0;
+}
+int top()
+{
+    return head->data;
+}
+int main()
+{
+    push(1);
+    push(2);
+    push(3);
+    push(4);
+    cout << pop() << " ";
+    cout << top();
+}
