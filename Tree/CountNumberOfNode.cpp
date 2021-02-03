14ff3df26eaf4658115b3953f6db4c1b8401a931
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-02-03 10:47:05 +0530

    Create CountNumberOfNode.cpp

---
+++ Tree/CountNumberOfNode.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+template <typename T>
+class TreeNode
+{
+public:
+    T data;
+    vector<TreeNode<T> *> children;
+
+    TreeNode(T data)
+    {
+        this->data = data;
+    }
+};
+void printTree(TreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << ":";
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        cout << root->children[i]->data << ",";
+    }
+    cout << endl;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        printTree(root->children[i]);
+    }
+}
+TreeNode<int> *takeinputlevelwise()
+{
+    int rootdata;
+    cout << "Enter Data" << endl;
+    cin >> rootdata;
+    TreeNode<int> *root = new TreeNode<int>(rootdata);
+    queue<TreeNode<int> *> pendingnodes;
+    pendingnodes.push(root);
+    while (pendingnodes.size() != 0)
+    {
+        TreeNode<int> *front = pendingnodes.front();
+        pendingnodes.pop();
+        cout << "Enter number of child of " << front->data << endl;
+        int numchild;
+        cin >> numchild;
+        for (int i = 0; i < numchild; i++)
+        {
+            int childata;
+            cout << "Enter " << i << "th child of " << front->data << endl;
+            cin >> childata;
+            TreeNode<int> *child = new TreeNode<int>(childata);
+            front->children.push_back(child);
+            pendingnodes.push(child);
+        }
+    }
+    return root;
+}
+int countnodes(TreeNode<int> *root)
+{
+    int ans = 1;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        ans += countnodes(root->children[i]);
+    }
+    return ans;
+}
+int main()
+{
+
+    TreeNode<int> *root = takeinputlevelwise();
+    cout << "Total Number of node is : " << countnodes(root) << endl;
+    printTree(root);
+}
