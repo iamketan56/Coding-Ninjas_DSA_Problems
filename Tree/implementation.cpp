87e8e90779551989afeb033ac27dd3e588b365a9
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-02-03 10:46:21 +0530

    Create implementation.cpp

---
+++ Tree/implementation.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+template <typename T>
+class TreeNode
+{
+public:
+    T data;
+    vector<TreeNode<T> *> children;
+
+    TreeNode(T data)
+    {
+        this->data = data;
+    }
+};
+void printTree(TreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << ":";
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        cout << root->children[i]->data << ",";
+    }
+    cout << endl;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        printTree(root->children[i]);
+    }
+}
+int main()
+{
+    TreeNode<int> *root = new TreeNode<int>(1);
+    TreeNode<int> *node1 = new TreeNode<int>(2);
+    TreeNode<int> *node2 = new TreeNode<int>(3);
+    TreeNode<int> *node3 = new TreeNode<int>(4);
+    root->children.push_back(node1);
+    root->children.push_back(node2);
+    root->children.push_back(node3);
+    printTree(root);
+}
