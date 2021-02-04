bb3df14c631bcc7a5c4625398b2e1edcac3e2894
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-02-04 07:33:50 +0530

    Create HeightOfaTree.cpp

---
+++ Tree/HeightOfaTree.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+template <typename T>
+class TreeNode
+{
+public:
+    T data;
+    vector<TreeNode<T> *> children;
+
+    TreeNode(T data)
+    {
+        this->data = data;
+    }
+};
+void printTree(TreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << ":";
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        cout << root->children[i]->data << ",";
+    }
+    cout << endl;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        printTree(root->children[i]);
+    }
+}
+TreeNode<int> *takeinputlevelwise()
+{
+    int rootdata;
+    cout << "Enter Data" << endl;
+    cin >> rootdata;
+    TreeNode<int> *root = new TreeNode<int>(rootdata);
+    queue<TreeNode<int> *> pendingnodes;
+    pendingnodes.push(root);
+    while (pendingnodes.size() != 0)
+    {
+        TreeNode<int> *front = pendingnodes.front();
+        pendingnodes.pop();
+        cout << "Enter number of child of " << front->data << endl;
+        int numchild;
+        cin >> numchild;
+        for (int i = 0; i < numchild; i++)
+        {
+            int childata;
+            cout << "Enter " << i << "th child of " << front->data << endl;
+            cin >> childata;
+            TreeNode<int> *child = new TreeNode<int>(childata);
+            front->children.push_back(child);
+            pendingnodes.push(child);
+        }
+    }
+    return root;
+}
+int height(TreeNode<int> *root)
+{
+    int max = 0;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        int test = height(root->children[i]);
+        if (max < test)
+        {
+            max = test;
+        }
+    }
+    return max + 1;
+}
+int main()
+{
+
+    TreeNode<int> *root = takeinputlevelwise();
+
+    printTree(root);
+    cout << "Height of this Tree is : " << height(root) << endl;
+}
