655975f0812ecda8ac6a699522b27061d1088359
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-02-06 08:00:14 +0530

    Create Implementaion.cpp

---
+++ Binary_Tree/Implementaion.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+template <typename T>
+class BinaryTreeNode
+{
+public:
+    T data;
+    BinaryTreeNode *left;
+    BinaryTreeNode *right;
+    BinaryTreeNode(T data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+    ~BinaryTreeNode()
+    {
+        delete left;
+        delete right;
+    }
+};
+void printBinaryTree(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " :";
+    if (root->left != NULL)
+    {
+        cout << "L" << root->left->data;
+    }
+    if (root->right != NULL)
+    {
+        cout << "R" << root->right->data;
+    }
+    cout << endl;
+
+    printBinaryTree(root->left);
+    printBinaryTree(root->right);
+}
+int main()
+{
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(1);
+    BinaryTreeNode<int> *node1 = new BinaryTreeNode<int>(2);
+    BinaryTreeNode<int> *node2 = new BinaryTreeNode<int>(3);
+    root->left = node1;
+    root->right = node2;
+    printBinaryTree(root);
+    delete root;
+}
