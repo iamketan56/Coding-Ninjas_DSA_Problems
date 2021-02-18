6b24f95f27f4ebc1d4c4c11cd5d54fc4bd3250b5
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-02-18 07:16:15 +0530

    Create BSF_Traversal.cpp

---
+++ Binary_Tree/BSF_Traversal.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int d)
+    {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+Node *buildTree()
+{
+    int d;
+    cin >> d;
+    if (d == -1)
+    {
+        return NULL;
+    }
+    Node *root = new Node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+void print(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    print(root->left);
+    print(root->right);
+}
+void BFS(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node *f = q.front();
+        cout << f->data << " " << endl;
+        q.pop();
+
+        if (f->left)
+        {
+            q.push(f->left);
+        }
+        if (f->right)
+        {
+            q.push(f->right);
+        }
+    }
+    return;
+}
+int main()
+{
+    Node *root = buildTree();
+    BFS(root);
+}
