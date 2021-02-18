844cafe1124011dec3bf8f17a4d9a44540ac684d
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-02-18 08:03:21 +0530

    Create Replace_Node_With_Sum.cpp

---
+++ Binary_Tree/Replace_Node_With_Sum.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int d)
+    {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+Node *buildTree()
+{
+    int d;
+    cin >> d;
+    if (d == -1)
+    {
+        return NULL;
+    }
+    Node *root = new Node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+void print(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    print(root->left);
+    print(root->right);
+}
+
+void BFS(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node *f = q.front();
+        cout << f->data << " ";
+        q.pop();
+
+        if (f->left)
+        {
+            q.push(f->left);
+        }
+        if (f->right)
+        {
+            q.push(f->right);
+        }
+    }
+    return;
+}
+int ReplaceSum(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    if (root->left == NULL && root->right == NULL)
+    {
+        return root->data;
+    }
+    int leftsum = ReplaceSum(root->left);
+    int rightsum = ReplaceSum(root->right);
+
+    int temp = root->data;
+    root->data = leftsum + rightsum;
+    return temp + root->data;
+}
+int main()
+{
+    Node *root = buildTree();
+    BFS(root);
+    cout << endl;
+    ReplaceSum(root);
+    BFS(root);
+}
