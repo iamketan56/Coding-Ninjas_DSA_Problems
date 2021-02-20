6ba6daa8931626d4e06a97be844c601b6fada4c5
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-02-20 06:52:22 +0530

    Update Check_ISBST.cpp

--- Binary Search Tree(BST)/Check_ISBST.cpp
+++ Binary Search Tree(BST)/Check_ISBST.cpp
@@ -1,106 +1,103 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-class Node
+template <typename T>
+class BinaryTreeNode
 {
 public:
-    int data;
-    Node *left;
-    Node *right;
-    Node(int d)
+    T data;
+    BinaryTreeNode *left;
+    BinaryTreeNode *right;
+    BinaryTreeNode(T data)
     {
-        data = d;
+        this->data = data;
         left = NULL;
         right = NULL;
     }
+    ~BinaryTreeNode()
+    {
+        delete left;
+        delete right;
+    }
 };
-Node *insertinbst(Node *root, int data)
+BinaryTreeNode<int> *takeInput()
+{
+    cout << "Enter Data" << endl;
+    int rootdata;
+    cin >> rootdata;
+    if (rootdata == -1)
+    {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootdata);
+    queue<BinaryTreeNode<int> *> pending;
+    pending.push(root);
+    while (pending.size() != 0)
+    {
+        BinaryTreeNode<int> *front = pending.front();
+        pending.pop();
+        cout << "Enter Left child of " << front->data << endl;
+        int leftchild;
+        cin >> leftchild;
+        if (leftchild != -1)
+        {
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(leftchild);
+            front->left = child;
+            pending.push(child);
+        }
+
+        cout << "Enter right child of " << front->data << endl;
+        int rightchild;
+        cin >> rightchild;
+        if (rightchild != -1)
+        {
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(rightchild);
+            front->right = child;
+            pending.push(child);
+        }
+    }
+    return root;
+}
+void printBinaryTree(BinaryTreeNode<int> *root)
 {
     if (root == NULL)
     {
-        return new Node(data);
+        return;
     }
-    if (data <= root->data)
+    cout << root->data << " :";
+    if (root->left != NULL)
     {
-        root->left = insertinbst(root->left, data);
+        cout << "L" << root->left->data;
     }
+    if (root->right != NULL)
+    {
+        cout << "R" << root->right->data;
+    }
+    cout << endl;
 
-    else
-    {
-        root->right = insertinbst(root->right, data);
-    }
+    printBinaryTree(root->left);
+    printBinaryTree(root->right);
+}
 
-    return root;
-}
-Node *Build()
-{
-    int d;
-    cin >> d;
-    Node *root = NULL;
-    while (d != -1)
-    {
-        root = insertinbst(root, d);
-        cin >> d;
-    }
-    return root;
-}
-bool IsBST(Node *root, int minv = INT_MIN, int maxv = INT_MAX)
+bool IsBST(BinaryTreeNode<int> *root, int minv = INT_MIN, int maxv = INT_MAX)
 {
     if (root == NULL)
     {
         return true;
     }
-    if (((root->data >= minv) && (root->data <= maxv)) && (IsBST(root->left, minv, root->data)) && (IsBST(root->right, root->data, maxv)))
+    if (root->data < minv || root->data > maxv)
+    {
+        return false;
+    }
+    if ((IsBST(root->left, minv, root->data - 1)) && (IsBST(root->right, root->data, maxv)))
     {
         return true;
     }
     return false;
 }
-
-void BFS(Node *root)
-{
-    queue<Node *> q;
-    q.push(root);
-    q.push(NULL);
-    while (!q.empty())
-    {
-        Node *f = q.front();
-        if (f == NULL)
-        {
-            cout << endl;
-            q.pop();
-            if (!q.empty())
-            {
-                q.push(NULL);
-            }
-        }
-        else
-        {
-            cout << f->data << " ";
-            q.pop();
-
-            if (f->left)
-            {
-                q.push(f->left);
-            }
-            if (f->right)
-            {
-                q.push(f->right);
-            }
-        }
-    }
-    return;
-}
 int main()
 {
-    Node *root = Build();
-    BFS(root);
-    if (IsBST(root))
-    {
-        cout << "Perfectly BST" << endl;
-    }
-    else
-    {
-        cout << "Nooooottt a   BST";
-    }
+    BinaryTreeNode<int> *root = takeInput();
+    cout << IsBST(root);
+    delete root;
 }
